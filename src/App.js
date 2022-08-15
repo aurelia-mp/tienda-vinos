83957a1d134c13038e4b463181df684d6bef5a61
aurelia-mp <aurelia.porolli@gmail.com>
2022-08-15 14:27:01 -0300

    creando la navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <NavBar/>
       </header>
+      <main>
+        <h1>En construcción</h1>
+      </main>
+      <footer className="footer">
+        <p>Footer</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () =>{
+    return(
+        <div className="navBar">
+            <div className="logo">
+                <a href="https://google.com" target="_blank" rel="noreferrer">Tienda de Vinos</a>
+            </div>
+            <div className="links">
+                <a href="https://google.com" target="_blank" rel="noreferrer">Quiénes somos</a>
+                <a href="https://google.com" target="_blank" rel="noreferrer">Envíos</a>
+                <a href="https://google.com" target="_blank" rel="noreferrer">Contacto</a>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
