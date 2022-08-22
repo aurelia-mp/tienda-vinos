8fc507744f36e035dac975fdd8e06f22eec1bb83
aurelia-mp <aurelia.porolli@gmail.com>
2022-08-22 15:02:10 -0300

    Componente ItemCount.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Header/NavBar";
+import ItemCount from "./components/ItemCount/ItemCount";
 
 function App() {
+  const onAdd = (cantidad, articulo) =>{
+    console.log(`${cantidad} ${articulo} agregados al carrito`)
+  }
+
   return (
     <div>
       <header>
         <NavBar/>
       </header>
       <ItemListContainer saludo='Bienvenido a Tienda de Vinos. Sitio en construcción' />
+      <ItemCount stock='10' initial='0' onAdd={onAdd}/>
       <footer className="footer">
         <p>Footer</p>
       </footer>
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [counter, setCounter] = useState(0)
+
+    const sumar = () => {
+        counter<stock ? setCounter(counter + 1) : alert("Stock insuficiente. Beber con moderación")
+    }
+
+    const restar = () => {
+        counter>initial && setCounter(counter - 1) 
+    }
+
+    // Se definen el nombre del articulo y su ID para generar dinámicamente el titúlo y foto
+    const itemName = "Luigi Bosca Malbec";
+    const itemId = 1;
+    
+  return (
+    <div className="counterBlock">
+        <h4>{itemName}</h4>
+        <img src={`../img/item${itemId}.jpeg`} alt={itemName}/>
+        <div className="counter">
+          <button className="counterButton" onClick={restar}>-</button>
+          <p>{counter}</p>
+          <button className="counterButton" onClick={sumar}>+</button>
+        </div>
+        <button disabled={counter=== 0? true:false} onClick={() => onAdd(counter, itemName)}>Agregar al Carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
