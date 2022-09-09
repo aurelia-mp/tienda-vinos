64475d7caa68221d1498328791dd69ae80dd84a4
aurelia-mp <aurelia.porolli@gmail.com>
2022-09-09 12:20:37 -0300

    Sincronizacion del cart Widget

--- src/components/Header/CartWidget.js
+++ src/components/Header/CartWidget.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
 
 const CartWidget = () => {
+  const {cart} = useContext(CartContext)
+  let totalItems = cart.reduce((x, y) => x + y.cantidad, 0)
+
   return (
     <div className='cart'>
         <span className="material-symbols-outlined">shopping_cart</span>
+        <p>{totalItems}</p>
     </div>
   )
 }
